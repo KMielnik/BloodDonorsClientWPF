fea2bcb1b79abd5c3ed78f54d06af3aebe10b6b0
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-08-10 22:21:28 +0200

    Added new dialogs

--- BloodDonorsClientWPF/MainWindow.xaml.cs
+++ BloodDonorsClientWPF/MainWindow.xaml.cs
@@ -23,32 +23,63 @@ namespace BloodDonorsClientWPF
     public partial class MainWindow : Window
     {
         private readonly ClientFactory clientFactory;
+        private readonly MiscellaneousClient miscellaneousClient;
 
         public MainWindow()
         {
             InitializeComponent();
             clientFactory = new ClientFactory();
             MainFrame.Content = new MainPage(clientFactory);
+
+            miscellaneousClient = clientFactory.GetMiscellaneousClient();
         }
 
         private void ShowServersAvailability(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            DialogHostIsServerOnline.IsOpen = true;
         }
 
         private void ShowAbout(object sender, RoutedEventArgs e)
         {
-            
+            DialogHostAbout.IsOpen = true;
         }
 
         private void ExitButtonClicked(object sender, RoutedEventArgs e)
-            => Close();
+        {
+            DialogHostExit.IsOpen = true;
+        }
         
 
-        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        private void HeaderOnMouseDown(object sender, MouseButtonEventArgs e)
         {
             if(e.LeftButton == MouseButtonState.Pressed)
                 this.DragMove();
+        }
+
+        private void ExitDialog_OnDialogClosing(object sender, DialogClosingEventArgs eventargs)
+        {
+            if(Equals(eventargs.Parameter,true))
+                Close();
+        }
+
+        private async void IsServerOnlineDialog_OnDialogOpening(object sender, DialogOpenedEventArgs eventargs)
+        {
+            ServerStatusProgressBar.Visibility = Visibility.Visible;
+            ServerStatus.Text = "";
+
+            var isServerOnline = await miscellaneousClient.IsServerOnline();
+
+            ServerStatusProgressBar.Visibility = Visibility.Collapsed;
+            if (isServerOnline)
+            {
+                ServerStatus.Text = "ONLINE";
+                ServerStatus.Foreground = Brushes.Green;
+            }
+            else
+            {
+                ServerStatus.Text = "OFFLINE";
+                ServerStatus.Foreground = Brushes.DarkRed;
+            }
         }
     }
 }
