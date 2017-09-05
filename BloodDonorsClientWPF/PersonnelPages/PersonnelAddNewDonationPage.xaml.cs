2bb955b676ffb0159dd1489fbcbd0767ae76b03a
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-09-05 14:26:42 +0200

    Added page for adding new donations by personnel

--- BloodDonorsClientWPF/PersonnelPages/PersonnelAddNewDonationPage.xaml.cs
+++ BloodDonorsClientWPF/PersonnelPages/PersonnelAddNewDonationPage.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BloodDonorsClientLibrary.Exceptions;
+using BloodDonorsClientLibrary.Models;
 using BloodDonorsClientLibrary.Services;
+using MaterialDesignThemes.Wpf;
 
 namespace BloodDonorsClientWPF.PersonnelPages
 {
@@ -27,6 +30,43 @@ namespace BloodDonorsClientWPF.PersonnelPages
         {
             InitializeComponent();
             personnelClient = clientFactory.GetPersonnelClient();
+
+            Loaded += PersonnelAddNewDonationPage_Loaded;
+        }
+
+        private void PersonnelAddNewDonationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            RegisterDonorSnackbar.MessageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(1200));
+        }
+
+        private async void AddDonationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var pesel = PeselTextBox.Text;
+            var result = int.TryParse(VolumeTextBox.Text,out int volume);
+
+            if (result == false)
+            {
+                RegisterDonorSnackbar.MessageQueue.Enqueue("PLEASE, enter a correct volume.");
+                return;
+            }
+
+            var donationDate = DonationDatePicker.SelectedDate ?? DateTime.Now;
+            var donationTime = DonationTimePicker.SelectedTime ?? DateTime.Now;
+
+            var donationDateTime = new DateTime(donationDate.Year,donationDate.Month,donationDate.Day,
+                                                donationTime.Hour,donationTime.Minute,donationTime.Second);
+
+            try
+            {
+                await personnelClient.AddDonationAsync(donationDateTime, volume, pesel);
+            }
+            catch (Exception)
+            {
+                RegisterDonorSnackbar.MessageQueue.Enqueue("Error");
+                return;
+            }
+
+            RegisterDonorSnackbar.MessageQueue.Enqueue("Success");
         }
     }
 }
