777ab5f74d33feb07b977dd56fe7bf2c70793dfe
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-09-05 11:13:59 +0200

    Added donations pages with filtering

--- BloodDonorsClientWPF/PersonnelPages/PersonnelDonationsPage.xaml.cs
+++ BloodDonorsClientWPF/PersonnelPages/PersonnelDonationsPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BloodDonorsClientLibrary.Models;
 using BloodDonorsClientLibrary.Services;
 
 namespace BloodDonorsClientWPF.PersonnelPages
@@ -22,11 +25,78 @@ namespace BloodDonorsClientWPF.PersonnelPages
     public partial class PersonnelDonationsPage : Page
     {
         private readonly PersonnelClient personnelClient;
+        private readonly MiscellaneousClient miscellaneousClient;
+
+        private ObservableCollection<BloodDonation> Donations { get; set; }
+
+        public ObservableCollection<BloodDonation> FilteredDonations { get; set; }
 
         public PersonnelDonationsPage(ClientFactory clientFactory)
         {
             InitializeComponent();
             personnelClient = clientFactory.GetPersonnelClient();
+            miscellaneousClient = clientFactory.GetMiscellaneousClient();
+
+            Donations = new ObservableCollection<BloodDonation>();
+            FilteredDonations = new ObservableCollection<BloodDonation>();
+
+            Loaded += PersonnelDonationsPage_Loaded;
+        }
+
+        private async void PersonnelDonationsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<BloodType> bloodTypes = await miscellaneousClient.GetBloodTypesAsync();
+            IEnumerable<BloodDonation> donations = await personnelClient.GetAllBloodAsync();
+
+            foreach (var bloodType in bloodTypes)
+                BloodTypeComboBox.Items.Add(bloodType);
+
+            foreach (var donation in donations)
+                Donations.Add(donation);
+            FilterDataGrid();
+        }
+
+
+        private void FilterGrid_KeyUp(object sender, KeyEventArgs e)
+        {
+            FilterDataGrid();
+        }
+
+        private void BloodTypeComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterDataGrid();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BloodTypeComboBox.SelectedItem = null;
+            DonorPeselTextBox.Text = "";
+            BloodTakerTextBox.Text = "";
+
+            FilterDataGrid();
+        }
+
+        private void FilterDataGrid()
+        {
+            IEnumerable<BloodDonation> temporaryFilteredDonations = Donations.AsEnumerable();
+
+            var bloodType = BloodTypeComboBox.SelectedItem;
+            var donorPesel = DonorPeselTextBox.Text;
+            var bloodTakerPesel = BloodTakerTextBox.Text;
+
+            if (bloodType != null)
+                temporaryFilteredDonations =
+                    temporaryFilteredDonations.Where(x => x.BloodType.ToString() == bloodType.ToString());
+            if (donorPesel != "")
+                temporaryFilteredDonations =
+                    temporaryFilteredDonations.Where(x => x.Donor.Pesel.Contains(donorPesel));
+            if (bloodTakerPesel != "")
+                temporaryFilteredDonations =
+                    temporaryFilteredDonations.Where(x => x.BloodTaker.Pesel.Contains(bloodTakerPesel));
+
+            FilteredDonations.Clear();
+            foreach (var temporaryFilteredDonation in temporaryFilteredDonations)
+                FilteredDonations.Add(temporaryFilteredDonation);
         }
     }
 }
